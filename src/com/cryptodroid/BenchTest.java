b243693823f8cce2e71fe64c5615388947917787
Sergey Melnikov <Melnikov.Sergey.V@gmail.com>
2013-03-02 01:03:27 +0400

    minor build improvements

--- src/com/cryptodroid/BenchTest.java
+++ src/com/cryptodroid/BenchTest.java
@@ -1,8 +1,5 @@
 package com.cryptodroid;
 
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
